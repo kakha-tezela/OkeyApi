662f55b615401e384c2872c6a629d4aa1ba2dfb6
gigaa <kakha_tezelashvili@yahoo.com>
2017-01-25 03:56:04 -0800

    feat(user,registration,validation): user registration validation failed

--- app/Http/Middleware/RegistrationMiddleware.php
+++ app/Http/Middleware/RegistrationMiddleware.php
@@ -24,19 +24,15 @@ class RegistrationMiddleware
                 'email'       => 'bail|required|email|unique:users',
                 'phone'       => 'bail|required|unique:users|digits_between:9,9|regex:/^5/',
                 'gender'      => 'bail|required|digits_between:1,1',
-                'year'        => 'required|digits_between:1,1',   
-                'month'       => 'required|digits_between:1,1',   
-                'day'         => 'required|digits_between:1,1',   
+                'birth_date'  => 'bail|required|date_format:"d/m/Y"',   
                 'address'     => 'required',
-                'city_id'     => 'bail|required|digits_between:1,1',
+                'city_id'     => 'bail|required|digits_between:1,4',
                 'pid'         => 'bail|unique:users,personal_id|digits_between:11,11',
-                'username'    => 'bail|unique:users,username',
+                'username'    => 'bail|required|unique:users,username',
                 'password'    => 'bail|required',
-                'company_id'  => 'bail|required|digits_between:1,1',
                 'social_id'   => 'bail|required|digits_between:1,1',
                 'work_place'  => 'bail|required',
                 'salary_id'   => 'bail|required|digits_between:1,1',
-                'status'      => 'bail|required|digits_between:1,1',
         ]);
 
         
@@ -50,6 +46,153 @@ class RegistrationMiddleware
             
             
             //Detect which rule caused fail
+            
+            if( isset( $failed['birth_date'] ) ):
+
+                if( isset( $failed['birth_date']['Required'] ) )
+                {
+                   return response()->json( "Birth Date is Missing", 400 );
+                }
+                elseif( isset( $failed['birth_date']['DateFormat'] ) )
+                {
+                   return response()->json( "Birth Date format is incorrect", 400 );
+                }
+
+            endif;
+            
+            
+            
+            
+            if( isset( $failed['salary_id'] ) ):
+
+                if( isset( $failed['salary_id']['Required'] ) )
+                {
+                   return response()->json( "Salary is Missing", 400 );
+                }
+                elseif( isset( $failed['salary_id']['DigitsBetween'] ) )
+                {
+                   return response()->json( "Salary length is incorrect", 400 );
+                }
+
+            endif;         
+
+            
+            
+            
+            
+            
+            if( isset( $failed['work_place'] ) ):
+                
+                if( isset( $failed['work_place']['Required'] ) )
+                {
+                   return response()->json( "Work Place is Missing", 400 );
+                }
+
+            endif;
+            
+            
+            
+            
+            
+            
+            
+            if( isset( $failed['social_id'] ) ):
+                
+                if( isset( $failed['social_id']['Required'] ) )
+                {
+                   return response()->json( "Social Status Missing", 400 );
+                }
+                elseif( isset( $failed['social_id']['DigitsBetween'] ) )
+                {
+                   return response()->json( "Social Status Lenght is incorrect", 400 );
+                }
+
+            endif;
+            
+            
+            
+            
+            
+            
+            
+            
+            if( isset( $failed['password'] ) ):
+                
+                if( isset( $failed['password']['Required'] ) )
+                   return response()->json( "Username Missing", 400 );
+                
+            endif;
+            
+            
+            
+            
+            
+            
+            if( isset( $failed['username'] ) ):
+                
+                if( isset( $failed['username']['Required'] ) )
+                {
+                   return response()->json( "Username Missing", 400 );
+                }
+                elseif( isset( $failed['username']['Unique'] ) )
+                {
+                   return response()->json( "Username Already in use", 400 );
+                }
+
+            endif;
+            
+            
+            
+            
+            
+            if( isset( $failed['city_id'] ) ):
+                
+                if( isset( $failed['city_id']['Required'] ) )
+                {
+                   return response()->json( "City Missing", 400 );
+                }
+                elseif( isset( $failed['city_id']['DigitsBetween'] ) )
+                {
+                   return response()->json( "City Length Is not Correct", 400 );
+                }
+
+            endif;
+            
+            
+            
+            
+            
+            
+            
+            
+            if( isset( $failed['address'] ) ):
+                
+                if( isset( $failed['address']['Required'] ) )
+                   return response()->json( "Address Missing", 400 );
+
+            endif;
+            
+            
+            
+            
+            
+            if( isset( $failed['gender'] ) ):
+                
+                if( isset( $failed['gender']['Required'] ) )
+                {
+                   return response()->json( "Gender Missing", 400 );
+                }
+                elseif( isset( $failed['gender']['DigitsBetween'] ) )
+                {
+                   return response()->json( "Gender Length Is not Correct", 400 );
+                }
+
+            endif;
+            
+            
+            
+            
+            
             
             if( isset( $failed['firstname'] ) ):
             
@@ -107,6 +250,8 @@ class RegistrationMiddleware
 
             
             
+            
+            
             if( isset( $failed['phone'] ) ):
                 
                 if( isset( $failed['phone']['Required'] ) )
@@ -127,8 +272,6 @@ class RegistrationMiddleware
                 }
 
             endif;
-            
-            
             
             
             
