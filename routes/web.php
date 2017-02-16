be70c96b388fb050e24c4dcb92ff3a0195f63bef
kakha_tezela <kakha_tezelashvili@yahoo.com>
2017-02-16 21:01:09 +0400

    plogin created

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
-<?php
 
+<?php
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -9,65 +9,42 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-
 // Personal Login
 Route::post('/plogin', 'PersonalController@login');
-
-
-
-
 
 // Account Check
 Route::post('/test', 'UserController@test');
 
-
 // Account Check
 Route::post('/check', 'AccountingController@accountSeederCheck');
-
 
 // Account Income
 Route::post('/account', 'AccountingController@emulator');
 
-
-
 // Create Schedule For User
 Route::post('/order/success', 'OrderController@createSchedule');
-
 
 // Create Annuity Schedule For User
 Route::post('annuity/order/success', 'OrderController@createAnnuitySchedule');
 
-
 Route::post('/orderstatus', 'OrderController@orderStatus');
-
-
-
-
-
 
 // Get invoice Information
 Route::post('/invoice', 'InvoiceController@getInvoiceInfo');
 
-
 // Check User With PID
 Route::post('/checkuser', 'UserController@checkUser');
-
 
 // User Registration
 Route::post('/register', 'UserController@register')->middleware('registration');
 
-
 // User Authorization
 Route::post('/login', 'UserController@login');
-
 
 //Check Token
 Route::post('/checktoken', 'UserController@getAuthenticatedUser');
 
-
 // Get User Data
 Route::post('/userdata', 'UserController@userData');
-
 
 Route::post('/okeyapi', 'MerchantController@processinvoice');
